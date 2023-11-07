57459cf0f2c1e171dde4c8a6ce07d26c5dc614e6
Chebet Kamara <chebetkamara@gmail.com>
2023-11-07 10:28:45 +0000

    Basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Auth class"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False - path and excluded_paths"""
+        suffix = '/'
+        path1 = ""
+        path2 = ""
+
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        while path:
+            if path.endswith(suffix):
+                path1 = path.rstrip('/')
+            else:
+                path2 = path + '/'
+            if path1 or path2 in excluded_paths:
+                return False
+            else:
+                return True
+
+    def authorization_header(self, request=None) -> str:
+        """returns request"""
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None - request"""
+        return None
