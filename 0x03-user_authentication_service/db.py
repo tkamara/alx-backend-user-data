17146e9010a07fbc45996890c14847050f76c6a3
Chebet Kamara <chebetkamara@gmail.com>
2023-11-16 13:56:57 +0000

    Fixing errors

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,9 +34,13 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """returns a User object"""
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
@@ -49,7 +53,7 @@ class DB:
             if arg not in columns:
                 raise InvalidRequestError
 
-        user = self._session.query(User).filter_by(**kwargs).first()
+        user = self._session.query(User).filter_by(**kwargs).one()
 
         if user is None:
             raise NoResultFound
