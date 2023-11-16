952d760ecf8cdf0d311b353a1298b4c8d6e7e933
Chebet Kamara <chebetkamara@gmail.com>
2023-11-16 14:25:45 +0000

    Fixing errors

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,13 +80,10 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """updates user's session ID to none"""
         try:
-            user = self._db.find_user_by(user_id=user_id)
-            if user:
-                user.session_id = None
-                self._db.update_user(user_id=user.id,
-                                     session_id=user.session_id)
-        except NoResultFound:
+            self._db.update_user(user_id=user.id, session_id=None)
             return None
+        except Exception:
+            return
 
     def get_reset_password_token(self, email: str) -> str:
         """dealing with reset token"""
